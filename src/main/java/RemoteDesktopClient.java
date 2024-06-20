cdd6a065184fe9af1af7bd7b997981e2b7045606
teien <45290596+teien@users.noreply.github.com>
2024-06-20 10:41:47 +0700

    fix remote

--- src/main/java/RemoteDesktopClient.java
+++ src/main/java/RemoteDesktopClient.java
@@ -9,6 +9,10 @@ import javax.swing.*;
 public class RemoteDesktopClient {
     private Socket socket;
     private DataInputStream dis;
+    private DataOutputStream dos;
+    private JFrame frame;
+    private JLabel label;
+    private BufferedImage currentImage;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new RemoteDesktopClient().start());
@@ -23,14 +27,16 @@ public class RemoteDesktopClient {
 
         try {
             socket = new Socket(ip, 12345);
-            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            dos = new DataOutputStream(socket.getOutputStream());
             dis = new DataInputStream(socket.getInputStream());
 
             System.out.println("Đã kết nối tới server");
 
-            JFrame frame = new JFrame("Remote Desktop Viewer");
-            JLabel label = new JLabel();
+            frame = new JFrame("Remote Desktop Viewer");
+            label = new JLabel();
             frame.add(label);
+            frame.setSize(800, 600);
+            frame.setLocationRelativeTo(null);
             frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
             frame.addWindowListener(new WindowAdapter() {
                 @Override
@@ -45,10 +51,9 @@ public class RemoteDesktopClient {
 
             frame.setVisible(true);
 
-            addMouseListeners(label, dos);
-            addKeyListeners(frame, dos);
+            addMouseListeners(label);
+            addKeyListeners(frame);
 
-            // Exit the loop on error
             SwingWorker<Void, BufferedImage> worker = new SwingWorker<>() {
                 @Override
                 protected Void doInBackground() throws Exception {
@@ -62,8 +67,11 @@ public class RemoteDesktopClient {
                                 ByteArrayInputStream bais = new ByteArrayInputStream(imageBytes);
                                 BufferedImage image = ImageIO.read(bais);
                                 publish(image);
+
+                                // Điều chỉnh tần suất nhận ảnh
+                                Thread.sleep(200); // Đợi 200ms trước khi nhận ảnh tiếp theo
                             }
-                        } catch (IOException e) {
+                        } catch (IOException | InterruptedException e) {
                             e.printStackTrace();
                             break; // Exit the loop on error
                         }
@@ -75,11 +83,6 @@ public class RemoteDesktopClient {
                 protected void process(java.util.List<BufferedImage> chunks) {
                     BufferedImage image = chunks.get(chunks.size() - 1);
                     label.setIcon(new ImageIcon(image));
-
-                    // Đặt kích thước của frame và label dựa trên kích thước của hình ảnh
-                    frame.setSize(image.getWidth(), image.getHeight());
-                    label.setSize(image.getWidth(), image.getHeight());
-
                     label.repaint();
                 }
 
@@ -94,46 +97,64 @@ public class RemoteDesktopClient {
             };
             worker.execute();
 
+            frame.addComponentListener(new ComponentAdapter() {
+                @Override
+                public void componentResized(ComponentEvent e) {
+                    updateImage();
+                }
+            });
+
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Lỗi kết nối tới server: " + e.getMessage());
             e.printStackTrace();
         }
     }
 
-    private void addMouseListeners(JLabel label, DataOutputStream dos) {
+    private void updateImage() {
+        if (currentImage != null) {
+            Dimension frameSize = frame.getContentPane().getSize();
+            if (frameSize.width > 0 && frameSize.height > 0) {
+                Image scaledImage = currentImage.getScaledInstance(frameSize.width, frameSize.height, Image.SCALE_SMOOTH);
+                label.setIcon(new ImageIcon(scaledImage));
+                label.repaint();
+            }
+        }
+    }
+
+    private void addMouseListeners(JLabel label) {
         MouseAdapter mouseAdapter = new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                sendMouseEvent("CLICK", e.getPoint(), dos, e.getButton());
+                sendMouseEvent("CLICK", e);
             }
 
             @Override
             public void mousePressed(MouseEvent e) {
-                sendMouseEvent("MOUSE_PRESS", e.getPoint(), dos, e.getButton());
+                sendMouseEvent("MOUSE_PRESS", e);
             }
 
             @Override
             public void mouseReleased(MouseEvent e) {
-                sendMouseEvent("MOUSE_RELEASE", e.getPoint(), dos, e.getButton());
+                sendMouseEvent("MOUSE_RELEASE", e);
             }
 
             @Override
             public void mouseMoved(MouseEvent e) {
-                sendMouseEvent("MOVE", e.getPoint(), dos, -1);
+                sendMouseEvent("MOVE", e);
             }
 
             @Override
             public void mouseDragged(MouseEvent e) {
-                sendMouseEvent("MOVE", e.getPoint(), dos, -1);
+                sendMouseEvent("MOVE", e);
             }
 
-            private void sendMouseEvent(String action, Point point, DataOutputStream dos, int button) {
+            private void sendMouseEvent(String action, MouseEvent e) {
                 try {
                     dos.writeUTF("CTL");
                     dos.writeUTF(action);
-                    dos.writeInt(point.x);
-                    dos.writeInt(point.y);
-                    dos.writeInt(button);
+                    dos.writeInt(e.getX());
+                    dos.writeInt(e.getY());
+                    dos.writeInt(e.getButton());
                     dos.flush();
                 } catch (IOException ex) {
                     ex.printStackTrace();
@@ -145,19 +166,19 @@ public class RemoteDesktopClient {
         label.addMouseMotionListener(mouseAdapter);
     }
 
-    private void addKeyListeners(JFrame frame, DataOutputStream dos) {
+    private void addKeyListeners(JFrame frame) {
         KeyAdapter keyAdapter = new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                sendKeyEvent("PRESS", e.getKeyCode(), dos);
+                sendKeyEvent("PRESS", e.getKeyCode());
             }
 
             @Override
             public void keyReleased(KeyEvent e) {
-                sendKeyEvent("RELEASE", e.getKeyCode(), dos);
+                sendKeyEvent("RELEASE", e.getKeyCode());
             }
 
-            private void sendKeyEvent(String action, int keyCode, DataOutputStream dos) {
+            private void sendKeyEvent(String action, int keyCode) {
                 try {
                     dos.writeUTF("CTL");
                     dos.writeUTF(action);
