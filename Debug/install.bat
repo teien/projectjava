0cd72d18643496e98f703e1aeea84697eb5b3313
teien <45290596+teien@users.noreply.github.com>
2024-07-22 22:43:25 +0700

    tien

--- Debug/install.bat
+++ Debug/install.bat
@@ -1,1 +1,6 @@
-sc create HardwareMonitorService binPath= "D:\Java\projectjava\Debug\HwSys.exe"
+@echo off
+set scriptDirectory=%~dp0
+set exeFileName=HwSys.exe
+set exePath=%scriptDirectory%%exeFileName%
+set serviceName=HardwareMonitorService
+sc create %serviceName% binPath= "%exePath%"
